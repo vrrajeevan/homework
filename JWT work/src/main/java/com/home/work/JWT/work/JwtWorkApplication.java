23c692c677c12575d5eeb1bd0197d948cdd301e8
vrrajeevan <vrrajeevan@vrrajeevan>
2018-04-22 22:02:37 -0400

    dev

---
+++ JWT work/src/main/java/com/home/work/JWT/work/JwtWorkApplication.java
@@ -0,0 +1,12 @@
+package com.home.work.JWT.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtWorkApplication.class, args);
+	}
+}
