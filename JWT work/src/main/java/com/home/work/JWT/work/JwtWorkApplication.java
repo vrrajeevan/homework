f9bbdd71c3c5b5878a526fd8df043b2318ab62dc
vrrajeevan <vrrajeevan@vrrajeevan>
2018-06-13 23:21:29 -0400

    dev

--- JWT work/src/main/java/com/home/work/JWT/work/JwtWorkApplication.java
+++ JWT work/src/main/java/com/home/work/JWT/work/JwtWorkApplication.java
@@ -1,8 +1,15 @@
 package com.home.work.JWT.work;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -10,10 +17,44 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableJpaRepositories // This annotation will enable JPA repositories. This will scan for Spring Data repositories in the annotated configuration class by default.
 public class JwtWorkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JwtWorkApplication.class, args);
-	}
 	
+		public static void main(String[] args) {
+			
+			ConfigurableApplicationContext ctx=SpringApplication.run(JwtWorkApplication.class, args);
+			
+			System.out.println("######## Active Profoiles"+Arrays.toString(ctx.getEnvironment().getActiveProfiles()));
+			
+			System.out.println("########### System Properties################");
+			
+			Properties props=System.getProperties();
+			props.list(System.out);
+			System.out.println("########### System Properties END################");
+			
+			
+			  ConfigurableEnvironment env = ctx.getEnvironment();
+		        printSources(env);
+		        System.out.println("---- System properties -----");
+		        printMap(env.getSystemProperties());
+		        System.out.println("---- System Env properties -----");
+		        printMap(env.getSystemEnvironment());
+			
+		}
+		
+		
+		private static void printSources (ConfigurableEnvironment env) {
+	        System.out.println("---- property sources ----");
+	        for (PropertySource<?> propertySource : env.getPropertySources()) {
+	            System.out.println("name =  " + propertySource.getName() + "\nsource = " + propertySource
+	                                .getSource().getClass()+"\n");
+	        }
+	    }
+
+	    private static void printMap (Map<?, ?> map) {
+	        map.entrySet()
+	           .stream()
+	           .forEach(e -> System.out.println(e.getKey() + " = " + e.getValue()));
+
+	    }
 	@Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
